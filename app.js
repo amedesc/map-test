6e513edc7d8f0db80e9b61eb77de94446b41af36
Amed Espinoza <amed.espinoza@fairplaylabs.com>
2017-11-28 14:43:51 -0600

    Eliminar el custom SVG marker, cambio de iconos de svg a png

--- app.js
+++ app.js
@@ -1,39 +1,3 @@
-CustomMarker.prototype = new google.maps.OverlayView();
-
-function CustomMarker(opts) {
-    this.setValues(opts);
-}
-
-CustomMarker.prototype.draw = function() {
-    var self = this;
-    var div = this.div;
-    if (!div) {
-        div = this.div = $('' +
-           // '<div class="pulse"></div>' +
-            '<div class="pulse-container">' +
-                '<div class="pulse-box">' +
-                    '<svg class="pulse-svg">' +
-                        '<circle class="circle first-circle" fill="#FF6347" cx="10" cy="10" r="10"></circle>' +
-                        '<circle class="circle second-circle" fill="#FF6347" cx="10" cy="10" r="10"></circle>' +
-                        '<circle class="circle third-circle" fill="#FF6347" cx="10" cy="10" r="10"></circle>' +
-                        '<circle class="circle" fill="#FF6347" cx="10" cy="10" r="10"></circle>' +
-                    '</svg>' +
-                '</div>' +
-            '</div>' +
-            '')[0];
-        this.circle = this.div.getElementsByClassName('circle');
-        div.style.position = 'absolute';
-        div.style.cursor = 'pointer';
-        var panes = this.getPanes();
-        panes.overlayImage.appendChild(div);
-    }
-    var point = this.getProjection().fromLatLngToDivPixel(this.position);
-    if (point) {
-        div.style.left = point.x + 'px';
-        div.style.top = point.y + 'px';
-    }
-};
-
 // Main function
 window.addEventListener('load', function () {
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -219,22 +183,14 @@ window.addEventListener('load', function () {
             }
         ]
     });
-    var marker2 = new CustomMarker({
-        position: new google.maps.LatLng(10, -84),
-        map: map,
-        icon : {
-            anchor: new google.maps.Point(0, 51),
-            size: new google.maps.Size(10, 10),
-        }
-    });
     var allSeisms = [],
         url = 'http://rsnapiusr.ucr.ac.cr/api/seisms/' +
             'getWebMapSeisms' +
             '?access_token=559aca63553be4973f58dbc1',
         addMarkers = function(seisms) {
-            var normalIcon = '../rsn-map 2.0/icons/markerV.svg',
-                midIcon = '../rsn-map 2.0/icons/markerN.svg',
-                dangerIcon ='../rsn-map 2.0/icons/markerR.svg';
+            var normalIcon = '../map-test/icons/markerV.png',
+                midIcon = '../map-test/icons/markerN.png',
+                dangerIcon ='../map-test/icons/markerR.png';
             seisms.forEach(function(seism, index, arr) {
                 seism.localDateTime = new Date($.now());
                 var icon = normalIcon;
@@ -244,35 +200,12 @@ window.addEventListener('load', function () {
                 if(seism.magnitude >= 5) {
                     icon = dangerIcon;
                 }
-                var marker = new SVGMarker({
+                var marker = new google.maps.Marker({
                     map: map,
                     position: new google.maps.LatLng(seism.lat, seism.lon),
-                    icon: {
-                        anchor: new google.maps.Point(0, 51),
-                        size: new google.maps.Size(17, 51),
-                        url: icon
-                    },
-                    infoWindow: {
-                        content: [
-                            '<b class="fecha_hora_infoWindow">Fecha y Hora Local: </b> <span class="fecha_hora_infoWindow">' + seism.localDateTime + '</span>',
-                            '<b class="fecha_hora_infoWindow">Magnitud: </b> <span class="fecha_hora_infoWindow">' + seism.magnitude + ' Mw'+ '</span>',
-                            '<b class="fecha_hora_infoWindow">Ubicaci&oacute;n: </b> <span class="fecha_hora_infoWindow">' + seism.local+ '</span>',
-                            '<b class="fecha_hora_infoWindow">Profundidad :</b> <span class="fecha_hora_infoWindow">' + seism.depth + ' km'+ '</span>',
-                            '<b class="fecha_hora_infoWindow">Latitud: </b> <span class="fecha_hora_infoWindow">' +
-                            (
-                                Math.abs(seism.lat) +
-                                (seism.lat < 0?' S': ' N')
-                            )+ '</span>',
-                            '<b class="fecha_hora_infoWindow">Longitud: </b> <span class="fecha_hora_infoWindow">' +
-                            (
-                                Math.abs(seism.lon) +
-                                (seism.lat < 0?' E': ' O')
-                            )+ '</span>',
-                            '<b class="fecha_hora_infoWindow">Localizaci&oacute;n: </b> <span class="fecha_hora_infoWindow">Revisada por ' +
-                            seism.agency+ '</span>'
-                        ].join('<br />')
-                    }
-                })               
+                    title: seism.local,
+                    icon: icon,
+                });         
             });
         };
     function getSeisms(){
---
+++ facemap.js
@@ -0,0 +1,403 @@
+CustomMarker.prototype = new google.maps.OverlayView();
+
+function CustomMarker(opts) {
+    this.setValues(opts);
+}
+
+CustomMarker.prototype.draw = function() {
+    var self = this;
+    var div = this.div;
+    if (!div) {
+        div = this.div = $('' +
+           // '<div class="pulse"></div>' +
+            '<div class="pulse-container">' +
+                '<div class="pulse-box">' +
+                    '<svg class="pulse-svg">' +
+                        '<circle class="circle first-circle" fill="#FF6347" cx="10" cy="10" r="10"></circle>' +
+                        '<circle class="circle second-circle" fill="#FF6347" cx="10" cy="10" r="10"></circle>' +
+                        '<circle class="circle third-circle" fill="#FF6347" cx="10" cy="10" r="10"></circle>' +
+                        '<circle class="circle" fill="#FF6347" cx="10" cy="10" r="10"></circle>' +
+                    '</svg>' +
+                '</div>' +
+            '</div>' +
+            '')[0];
+        this.circle = this.div.getElementsByClassName('circle');
+        div.style.position = 'absolute';
+        div.style.cursor = 'pointer';
+        var panes = this.getPanes();
+        panes.overlayImage.appendChild(div);
+    }
+    var point = this.getProjection().fromLatLngToDivPixel(this.position);
+    if (point) {
+        div.style.left = point.x + 'px';
+        div.style.top = point.y + 'px';
+    }
+};
+
+// Main function
+window.addEventListener('load', function () {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 8,
+        center: new google.maps.LatLng(9.4, -84),
+        styles: [
+            {
+                "featureType": "all",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#f5f5f5"
+                    }
+                ]
+            },
+            {
+                "featureType": "all",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#616161"
+                    }
+                ]
+            },
+            {
+                "featureType": "all",
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                    {
+                        "color": "#f5f5f5"
+                    }
+                ]
+            },
+            {
+                "featureType": "all",
+                "elementType": "labels.icon",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative.country",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#263238"
+                    },
+                    {
+                        "weight": "1.20"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative.province",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#546e7a"
+                    },
+                    {
+                        "weight": "1.00"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative.land_parcel",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#bdbdbd"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape.natural.terrain",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#bfc2cb"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape.natural.terrain",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#e03229"
+                    },
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#eeeeee"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#757575"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#e5e5e5"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#9e9e9e"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#ffffff"
+                    },
+                    {
+                        "visibility": "on"
+                    },
+                    {
+                        "weight": "0.50"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    },
+                    {
+                        "hue": "#17ff00"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#dadada"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#ffffff"
+                    },
+                    {
+                        "weight": 2
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#ffffff"
+                    },
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#616161"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway.controlled_access",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#757575"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#9e9e9e"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit.line",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#e5e5e5"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit.station",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#eeeeee"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#c9c9c9"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#9e9e9e"
+                    }
+                ]
+            }
+        ]
+    });
+    var marker2 = new CustomMarker({
+        position: new google.maps.LatLng(10, -84),
+        map: map,
+        icon : {
+            anchor: new google.maps.Point(0, 51),
+            size: new google.maps.Size(10, 10),
+        }
+    });
+    var allSeisms = [],
+        url = 'http://rsnapiusr.ucr.ac.cr/api/seisms/' +
+            'getWebMapSeisms' +
+            '?access_token=559aca63553be4973f58dbc1',
+        addMarkers = function(seisms) {
+            var icon1 = '../map-test/icons/1.png',
+                icon2 = '../map-test/icons/2.png',
+                icon3 ='../map-test/icons/3.png',
+                icon4 = '../map-test/icons/4.png',
+                icon5 = '../map-test/icons/5.png',
+                icon6 = '../map-test/icons/6.png',
+                icon7 = '../map-test/icons/7.png',
+                icon8 = '../map-test/icons/8.png',
+                icon9 = '../map-test/icons/9.png',
+                icon10 = '../map-test/icons/10.png';
+            seisms.forEach(function(seism, index, arr) {
+                seism.localDateTime = new Date($.now());
+                var icon = icon1;
+                if(seism.magnitude > 2.5) {
+                    icon = icon3;
+                }
+                if(seism.magnitude >= 3) {
+                    icon = icon4;
+                }
+                if(seism.magnitude >= 4) {
+                    icon = icon5;
+                }
+                if(seism.magnitude >= 5) {
+                    icon = icon6;
+                }
+                var marker = new google.maps.Marker({
+                    map: map,
+                    position: new google.maps.LatLng(seism.lat, seism.lon),
+                    title: seism.local,
+                    icon: icon,
+                });         
+            });
+        };
+    function getSeisms(){
+        $.get(url).then(function(result) {
+                allSeisms = result.seisms;
+                addMarkers(allSeisms);
+        }, function(error) {
+            console.error(error);
+        });
+    }
+    getSeisms();
+});
